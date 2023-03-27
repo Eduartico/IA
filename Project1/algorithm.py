996e1f637010dfbe20688f16defc9ca64e90faa1
Anete <anetempereira@gmail.com>
2023-03-27 10:07:59 +0100

    initial version of greedy and uniform without being adapted to our problem

--- Project1/algorithm.py
+++ Project1/algorithm.py
@@ -1,10 +1,28 @@
 import heapq # we'll be using a heap to store the states
 
 
-def greedy_search(initial_state, heuristic):
-    # problem (NPuzzleState) - the initial state
-    # heuristic (function) - the heuristic function that takes a board (matrix), and returns an integer
-    states = [initial_state]
+
+boardAlmostWin = [
+    [' ', ' ', ' ', 'B', 'R'],
+    ['R', 'B', 'B', 'R', ' '],
+    ['G', 'G', 'G', ' ', 'G'],
+    ['G', 'B', 'B', 'B', 'G'],
+    ['R', ' ', 'R', ' ', 'R']
+]
+
+def getListOfMoves(board):
+    moves = []
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if board[row][col] == ' ':
+                moves.append([row, col])
+    return moves
+
+
+def greedy_search(initial_board, heuristic):
+    # initial_board (SymmetryPuzzle) - the initial state
+    # heuristic (function) - the heuristic function that takes a board (matrix), and returns an integer saying how many rows and columns are left to complete
+    states = [initial_board]
     visited = set() 
 
     setattr(SymmetryPuzzle, "__lt__", lambda self, other: heuristic(self) < heuristic(other))
@@ -21,4 +39,27 @@ def greedy_search(initial_state, heuristic):
             if child_state not in visited:
                 heapq.heappush(states, child_state)
         
+    return None
+
+
+
+def uniform_cost(initial_board):
+    # initial_board(SymmetryPuzzle) - the initial state
+    queue = [initial_board]
+    visited = set() # to not visit the same state twice
+    
+    while queue:
+        board = queue.pop(0)
+        if board in visited:
+            continue
+        else:
+            visited.add(board)
+
+        if board.is_complete():   # check goal state
+            return board.move_history
+        
+        # make moves
+        moves = board.children()
+        for move in moves:
+            queue.append(move)
     return None
