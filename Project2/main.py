a581b5eeb4f648cf18ae3b54b23ed4fa90d8c256
up202004999 <duarte.eduardoeds@gmail.com>
2023-05-16 11:28:17 +0100

    added encoding

--- Project2/main.py
+++ Project2/main.py
@@ -8,7 +8,9 @@ from sklearn.svm import SVR
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestRegressor
 from enum import Enum
+from sklearn.preprocessing import LabelEncoder
 import os
+
 class ModelType(Enum):
     svm = 1
     randforest = 2
@@ -26,25 +28,43 @@ model_map = {
 }
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-filename = "airlines_delay.csv"
+filename = "airlines_delay_original.csv"
 data = pd.read_csv(os.path.join(current_dir, filename))
-#data = pd.read_csv("airlines_delay.csv")
-#data = pd.read_csv("iris-data.csv")
+data_encoded = data.copy()
+
+# Combine "AirportFrom" and "AirportTo" columns to create a unified set of airports
+airports = pd.concat([data_encoded['AirportFrom'], data_encoded['AirportTo']])
+
+# Perform label encoding on the combined set of airports
+label_encoder_airports = LabelEncoder()
+encoded_airports = label_encoder_airports.fit_transform(airports)
+
+# Perform label encoding on the "Airline" column
+label_encoder_airline = LabelEncoder()
+data_encoded['Airline'] = label_encoder_airline.fit_transform(data_encoded['Airline'])
+
+# Update "AirportFrom" and "AirportTo" columns with the encoded values
+data_encoded['AirportFrom'] = encoded_airports[:len(data_encoded)]
+data_encoded['AirportTo'] = encoded_airports[len(data_encoded):]
+
+# Remove the "Flight" column
+data_encoded = data_encoded.drop('Flight', axis=1)
+
+# Save the encoded data to a CSV file
+data_encoded.to_csv('airlines_delay_encoded.csv', index=False)
 
 # train and test svm model
-data_manip.train_model(data, model_map[ModelType.svm][0], model_map[ModelType.svm][1])
+data_manip.train_model(data_encoded, model_map[ModelType.svm][0], model_map[ModelType.svm][1])
 model_test.test_model(model_map[ModelType.svm][1])
 
 # train and test logical regression model
-data_manip.train_model(data, model_map[ModelType.randforest][0], model_map[ModelType.randforest][1])
+data_manip.train_model(data_encoded, model_map[ModelType.randforest][0], model_map[ModelType.randforest][1])
 model_test.test_model(model_map[ModelType.randforest][1])
 
 # train and test gradient booster regressor model
-data_manip.train_model(data, model_map[ModelType.graboo][0], model_map[ModelType.graboo][1])
+data_manip.train_model(data_encoded, model_map[ModelType.graboo][0], model_map[ModelType.graboo][1])
 model_test.test_model(model_map[ModelType.graboo][1])
 
 # train and test MLPRegressor model
-data_manip.train_model(data, model_map[ModelType.MLPR][0], model_map[ModelType.MLPR][1])
+data_manip.train_model(data_encoded, model_map[ModelType.MLPR][0], model_map[ModelType.MLPR][1])
 model_test.test_model(model_map[ModelType.MLPR][1])
-
-
